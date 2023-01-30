36e07128118994a50621c5440f6d4cb6a43248a4
Ghilardi.19319 <Ghilardi.19319@LABI1-6PC07>
2023-01-30 10:38:11 +0100

    pt.2

--- Ecommerce2/Alimentare.cs
+++ Ecommerce2/Alimentare.cs
@@ -8,6 +8,67 @@ namespace Ecommerce2
 {
     public class Alimentare : Prodotto
     {
+        private DateTime _scadenza;
+        private decimal _prezzoscontato;
+        private string[] _ingredienti;
 
+        public Alimentare(string id, string nome, string prod, decimal prezzo, string descr, DateTime scadenza) : base(id, nome, prod, descr, prezzo)
+        {
+            Scadenza = scadenza;
+            PrezzoScontato = prezzo;
+            _ingredienti = new string[10];
+        }
+
+        public DateTime Scadenza
+        {
+            get
+            {
+                return _scadenza;
+            }
+            private set
+            {
+                if (value > DateTime.Today)
+                    _scadenza = value;
+                else
+                    throw new Exception("Inserire un codice valido");
+            }
+        }
+
+        public decimal PrezzoScontato
+        {
+            get
+            {
+                return _prezzoscontato;
+            }
+            set
+            {
+                if (value > 0)
+                    _prezzoscontato = value;
+                else
+                    throw new Exception("Il prezzo deve essere positivo");
+            }
+        }
+
+        public void Applicare()
+        {
+            if (DateTime.Today.DayOfWeek - Scadenza.DayOfWeek < 7)
+            {
+                PrezzoScontato = 0.5m * Prezzo;
+            }
+        }
+
+        public void CaricareIngredienti(string a, string b, string c, string d, string e, string f, string g, string h, string i, string l)
+        {
+            _ingredienti[0] = a;
+            _ingredienti[1] = b;
+            _ingredienti[2] = c;
+            _ingredienti[3] = d;
+            _ingredienti[4] = e;
+            _ingredienti[5] = f;
+            _ingredienti[6] = g;
+            _ingredienti[7] = h;
+            _ingredienti[8] = i;
+            _ingredienti[9] = l;
+        }
     }
 }
---
+++ Ecommerce2/Cancelleria.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce2
+{
+    internal class Cancelleria : Prodotto
+    {
+        public int _grammatura;
+        public string _funzionamento;
+
+        public Cancelleria(string id, string nome, string prod, decimal prezzo, string descr, int grammatura, string funzionamento) : base(id, nome, prod, descr, prezzo)
+        {
+            Grammatura = grammatura;
+            Funzionamento = funzionamento;
+        }
+
+        public int Grammatura
+        {
+            get
+            {
+                return _grammatura;
+            }
+            set
+            {
+                if (value > 0)
+                    _grammatura = value;
+                else
+                    throw new Exception("Grammatura errata");
+            }
+        }
+
+        public string Funzionamento
+        {
+            get
+            {
+                return _funzionamento;
+            }
+            private set
+            {
+                if (value != null)
+                    _funzionamento = value;
+                else
+                    throw new Exception("Inserire un funzionamento valido");
+            }
+        }
+
+        public decimal PrezzoScontato()
+        {
+            decimal prezzoScontato = Prezzo;
+            if (DateTime.Today.Day % 2 == 0)
+                prezzoScontato -= Prezzo * 0.03m;
+            return prezzoScontato;
+        }
+    }
+}
